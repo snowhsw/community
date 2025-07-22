7489b2620ba2c5e0945468821bd4272dc9fd13ad
snowhsw <snowhswasd@naver.com>
2025-07-22 18:05:39 +0900

    modified:   components/Footer.js modified:   components/Footer.module.css

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import style from "./Footer.module.css"
 
 const Footer = () =>{
     return(
-        <footer>
-
+        <footer className={style.footer}>
+            <h1>풋터</h1>
         </footer>
     )
 }
